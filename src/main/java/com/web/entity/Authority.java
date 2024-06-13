8853eb05a29bc6a1cdf3a9dc41358d732ade6c63
Huy Thanh <22521361@gm.uit.edu.vn>
2024-06-13 14:21:16 +0700

    commit

--- src/main/java/com/web/entity/Authority.java
+++ src/main/java/com/web/entity/Authority.java
@@ -1,3 +1,22 @@
+// package com.web.entity;
+
+// import lombok.Getter;
+// import lombok.Setter;
+// import lombok.ToString;
+
+// import javax.persistence.*;
+
+// @Entity
+// @Table(name = "authority")
+// @Getter
+// @Setter
+// @ToString
+// public class Authority {
+
+//     @Id
+//     private String name;
+
+// }
 package com.web.entity;
 
 import lombok.Getter;
